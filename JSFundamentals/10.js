007229f4faeb989edd802764fed98ec3d80207fc
jwangr <jwanggr@gmail.com>
2025-06-21 09:43:09 +1000

    Submit 1.10

---
+++ JSFundamentals/10.js
@@ -0,0 +1,21 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.human = true;
+  this.canDrive = age >= 18 ? "yes" : "no";
+}
+
+let smeagol = new Person("Smeagol", 589);
+let bilbo = new Person("Bilbo Baggins", 111);
+
+console.log(`${JSON.stringify(smeagol)} \n${JSON.stringify(bilbo)}`);
+
+function PersonClass(name, color, dob) {
+  this.name = name;
+  this.color = color;
+  this.tree = true;
+  this.DOB = dob.toString();
+}
+
+let treebeard = new PersonClass("Treebeard", "brown", new Date());
+console.log(treebeard);
