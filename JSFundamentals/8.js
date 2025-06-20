36b31bb0c8ff36fc09064d1f1d859a8f9c9f80fd
jwangr <jwanggr@gmail.com>
2025-06-21 09:02:46 +1000

    Submit 1.8

--- JSFundamentals/8.js
+++ JSFundamentals/8.js
@@ -6,10 +6,18 @@ const sydney = {
   timezone: "Australia/Sydney",
 };
 
+const venice = {
+  name: "Venice",
+  population: 249_466,
+  state: "Veneto",
+  founded: "March 25, 421 AD",
+  timezone: "Central European Time"
+}
 printObject(sydney);
-
+printObject(venice);
 function printObject(city) {
   for (key in city) {
     console.log(`${key.slice(0,1).toUpperCase()}${key.slice(1)}: ${city[key]}`);
   }
 }
+
