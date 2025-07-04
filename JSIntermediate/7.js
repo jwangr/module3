88c991f5e9772f3b23a846417af92013b0b50442
jwangr <jwanggr@gmail.com>
2025-07-05 08:38:19 +1000

    Submit 2.7 v2

--- JSIntermediate/7.js
+++ JSIntermediate/7.js
@@ -11,8 +11,60 @@ function getBookTitle(bookId) {
         let items = books.find(book => book.id == bookId);
         return items.title;
     }
-    catch (error) { console.log(`\nBookId ${bookId} not found!`) }
+    catch (error) { console.log(`\nBookId ${bookId} not found!\n`) }
 }
+
+// function getBookTitle(bookId) {
+//     let item = books.find(book => book.id == bookId);
+//     return item?.title ? item.title : "";
+// }
 
 console.log(getBookTitle(1), getBookTitle(4), getBookTitle(7))
 // BookId 7 not found! The Great Gatsby Brave New World undefined
+
+function getOldBooks() {
+    let oldiesButGoldies = books.filter(book => book.year < 1950);
+    return oldiesButGoldies;
+}
+
+console.log(getOldBooks());
+
+let editGenre = getOldBooks().map(addGenre);
+// process each book
+function addGenre(book) {
+    let newBook = { ...book, genre: 'classic' };
+    return newBook;
+}
+// Display final array of objects
+console.log(editGenre);
+
+function getTitles(authorInitial) {
+    let pattern1 = authorInitial.toLowerCase()
+    let authorsBooks = books.filter(book => book.author.toLowerCase().startsWith(pattern1));
+    return authorsBooks;
+}
+
+console.log(getTitles('ge'), getTitles('a'));
+
+function getTitles2(authorInitial) {
+    const pattern1 = new RegExp(`\^${authorInitial}`, 'i')
+    let authorsBooks = books.filter(book => { pattern1.test(book.author) });
+    return authorsBooks;
+}
+
+// console.log(getTitles2('ge'), getTitles2('a'));
+
+function latestBook() {
+    let newestYear =0;
+    // foreach of the new books, find a book that has a newer date of publication
+    books.forEach(book => {
+        if (book.year > newestYear) {
+            newestYear = book.year
+        }
+    })
+
+    let newestBook = books.find(book => book.year == newestYear);
+    return newestBook;
+}
+
+console.log(latestBook());
