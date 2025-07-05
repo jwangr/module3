06c9458c6557ce9ee1429c54756e5d53da8ca657
jwangr <jwanggr@gmail.com>
2025-07-05 12:39:34 +1000

    Submit 2.9

--- JSIntermediate/9.js
+++ JSIntermediate/9.js
@@ -6,6 +6,24 @@ let salaries = {
 "James" : 43000
 };
 
+let salaryMap = new Map(Object.entries(salaries));
+
 function sumSalaries(salaries) {
-    salaries.forEach(salary => );
+    let sum = 0;
+    for (let salary of salaryMap.values()) {
+        sum += salary;
+    }
+    return sum;
 }
+
+function topEarner(salaries) {
+    const highestSalaries = Object.values(salaries).sort().reverse();
+    const highestEarnings = highestSalaries[0];
+    for (let salary of salaryMap.entries()) {
+        if (salary[1] == highestEarnings) {return salary[0]};
+    }
+}
+
+console.log(sumSalaries(salaries));
+
+console.log(topEarner(salaries));
