a7b657f25422571093f8d1c99601293e70a860f8
jwangr <jwanggr@gmail.com>
2025-07-05 15:33:09 +1000

    Submit 3.4

--- JSAdvanced/1.js
+++ JSAdvanced/1.js
@@ -7,8 +7,8 @@ function makeCounter(startFrom, incrementBy) {
     };
 }
 let counter1 = makeCounter(3, 0.5);
-counter1(); // 1
-counter1(); // 2
+counter1(); // 3.5
+counter1(); // 4
 
 let counter2 = makeCounter(10, 20);
-counter2();
+counter2(); // 30
--- JSAdvanced/4.js
+++ JSAdvanced/4.js
@@ -1,12 +1,21 @@
 //  e.g. 1, 1, 2, 3, 5, 8, 13, 21, 34, etc.
-let increment = 0;
-printFibonnaci.counter = 1;
-function printFibonnaci() {
-    printFibonnaci.counter += increment;
-    console.log(printFibonnaci.counter);
-    increment = printFibonnaci.counter;
+function increment() {
+    // initiate the starting values
+    let incrementor = 0;
+    let num1 = 1;
+
+    // the output is a function that returns the incremented num1, and changes the increment value
+    return function () {
+        console.log(num1);
+        num1 += incrementor;
+        incrementor = num1 - incrementor;
+        return num1, incrementor;
+    }
 }
 
+let printFibonnaci = increment(); // this should initiate num 1 = 1; increment = 0;
+// and printFibonnaci becomes the return value, ie. function that increments num1 by 1; and sets the incre
 
 let fibbonanza = setInterval( () => printFibonnaci(), 1000 ); // prints another Fibonnaci number every 1s
+
 setTimeout( () => clearInterval(fibbonanza), 10*1000);
