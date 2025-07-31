92b214ef4f58372e08e721094d84f3ecf6761fa3
jwangr <jwanggr@gmail.com>
2025-08-01 07:20:41 +1000

    Edit

--- project/capstone/hearthub/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import WaterIntake, Weight, Symptoms
-
-# Register your models here.
-admin.site.register(WaterIntake)
-admin.site.register(Weight)
-admin.site.register(Symptoms)
